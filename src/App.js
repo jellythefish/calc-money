daa93c39b2c4a1139b2eb3283f6c5a9370639b73
Svyatoslav Krasnov <beatcazzze@gmail.com>
2020-08-15 14:47:08 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Total from './components/total/Total';
+import History from './components/history/History';
+import Operation from './components/operation/Operation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    transactions: JSON.parse(localStorage.getItem('transactions')) || [],
+    description: '',
+    amount: '',
+    income: JSON.parse(localStorage.getItem('income')) || 0,
+    expenses: JSON.parse(localStorage.getItem('expenses')) || 0,
+  }
+
+  addTransaction = add => {
+    const transactions = [...this.state.transactions]
+
+    transactions.push({
+      id: `cmr${(+new Date()).toString(16)}`,
+      description: this.state.description,
+      amount: this.state.amount,
+      add
+    });
+
+    this.setState({ 
+      transactions, 
+      description: '', 
+      amount: '',
+      income: add * this.state.amount + this.state.income,
+      expenses: !add * this.state.amount + this.state.expenses 
+    }, this.addStorage)
+  }
+
+  deleteTransaction = (id) => {
+    let add = false;
+    let amount = 0;
+    const transactions = this.state.transactions.filter(function(item) {
+      if (item.id === id) {
+        add = item.add;
+        amount = item.amount;
+      }
+      return item.id !== id
+    });
+    this.setState({ 
+      transactions, 
+      income: this.state.income - add * amount,
+      expenses: this.state.expenses - !add * amount, 
+    })
+  }
+
+  addAmount = elem => {
+    this.setState({amount: elem.target.value });
+  }
+
+  addDescription = elem => {
+    this.setState({description: elem.target.value})
+  }
+
+  addStorage() {
+    localStorage.setItem('transactions', JSON.stringify(this.state.transactions));
+    localStorage.setItem('income', JSON.stringify(this.state.income));
+    localStorage.setItem('expenses', JSON.stringify(this.state.expenses));
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+            <h1>Кошелек</h1>
+            <h2>Калькулятор расходов</h2>
+        </header>
+  
+        <main>
+          <div className="container">
+            <Total 
+              income={this.state.income} 
+              expenses={this.state.expenses}
+            />
+            <History
+              transactions={this.state.transactions}
+              deleteTransaction={this.deleteTransaction}
+            />
+            <Operation 
+              addTransaction={this.addTransaction}
+              addAmount={this.addAmount}
+              addDescription={this.addDescription}
+              description={this.state.description}
+              amount ={this.state.amount}
+            />
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/history/HistoryItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HistoryItem = ({ transaction, deleteTransaction }) => (
+	<li className={`history__item history__item-${transaction.add ? "plus" : "minus"}`}>{transaction.description}
+		<span className="history__money">{`${transaction.add ? "+" : "-"}`}{transaction.amount} ₽</span>
+		<button 
+			className="history__delete"
+			onClick={() => deleteTransaction(transaction.id)}
+		>x</button>
+	</li>
+);
+
+export default HistoryItem;
